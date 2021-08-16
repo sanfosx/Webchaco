4a395d5d315725c4996524aa89b9d06ac5d577c9
sanfosx <sanfosx@hotmail.com>
2021-08-16 04:05:42 -0300

    conf

---
+++ src/MiSitio/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+def ini(request):
+	template_name="ini.html"
+	
+	return render(request,template_name)
+
+def inicio(request):
+	template_name="inicio.html"
+
+	lista_alumnos = [
+		"alumno 1",
+		"alumno 2"
+	]
+
+	ctx={
+		'username': "lucas",
+		"lista": lista_alumnos
+	}
+	return render(request,template_name, ctx )
+
+def login(request):
+	template_name="login.html"
+
+	ctx={
+
+	}
+	return render(request,template_name, ctx )
